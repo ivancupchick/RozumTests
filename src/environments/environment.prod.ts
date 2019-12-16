abfdce9c06a04e9b2ae084d29b303ddc1b20d532
Ivan Kupchyk <ivan.kupchic@gmail.com>
2019-12-16 18:26:17 +0300

    Fixed prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAvI7dd-178gwboYSbO9SFAuz8SL6aNRxA',
+    authDomain: 'questionsystem-890fb.firebaseapp.com',
+    databaseURL: 'https://questionsystem-890fb.firebaseio.com',
+    projectId: 'questionsystem-890fb',
+    storageBucket: '',
+    messagingSenderId: '1003583062619',
+    appId: '1:1003583062619:web:1a4e9f9d5cf38ded59b57e'
+  }
 };
